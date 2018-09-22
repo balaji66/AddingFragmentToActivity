07abe160d687f232aa5630b5c0444c5646d91f91
Balaji66 <pusuluribalaji66@gmail.com>
2018-09-22 23:47:11 +0530

    final commit

--- app/src/main/java/com/durga/balaji66/addingfragmenttoactivity/MainActivity.java
+++ app/src/main/java/com/durga/balaji66/addingfragmenttoactivity/MainActivity.java
@@ -2,6 +2,8 @@ package com.durga.balaji66.addingfragmenttoactivity;
 
 import android.content.Context;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -11,11 +13,11 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        context=this;
         setContentView(R.layout.activity_main);
-//        Fragment fragment = new BlankFragment();
-//        android.support.v4.app.FragmentManager fragmentManager = getSupportFragmentManager();
-//        fragmentManager.beginTransaction().replace(R.id.fragment,fragment).commit();
+
+        Fragment fragment = new BlankFragment();
+        android.support.v4.app.FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction().replace(R.id.fragment,fragment).commit();
 
     }
 }
